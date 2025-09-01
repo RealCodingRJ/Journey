6f3b347d65dd414e8cc3039eddce4a90b9d0241f
RealCodingRJ <ryangregoire286@gmail.com>
2025-09-01 15:05:37 -0400

    Added Both ArrayList and Some OOP Concepts

---
+++ C#-Features/ArrayList/Sample/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+
+namespace Samnpe
+{
+    class App
+    {
+
+        static void Main(string[] args)
+        {
+            ArrayList list = [];
+            list.Add("Ryan");
+            list.Add("Learning CSharp");
+            list.Add("Loser");
+
+            foreach (string lists in list)
+            {
+                Console.WriteLine(lists);
+
+            }
+
+        }
+    }
+}
---
+++ C#-Features/Inheratence/Inheritance/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Program
+{
+    class Person
+    {
+        public string? name;
+        public string? lastName;
+
+        public void RenderText(string firstName, string lastName)
+        {
+            this.name = firstName;
+            this.lastName = lastName;
+
+            Console.WriteLine($"First Name: {firstName}");
+            Console.WriteLine($"Last Name: {lastName}");
+        }
+
+    }
+
+    class User : Person
+    {
+
+        public User()
+        {
+            Console.WriteLine("Hello Usere..");
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            User user = new();
+            user.RenderText("Ryan", "James");
+        }
+    }
+
+}
