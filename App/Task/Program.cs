7176356823b33ef28776ab8fd4f1187ffa104373
RealCodingRJ <ryangregoire286@gmail.com>
2025-09-01 15:53:06 -0400

    Added Threads And Task

---
+++ App/Task/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace App
+{
+    class App
+    {
+        static async void Thread(byte second)
+        {
+
+            await Task.Delay(TimeSpan.FromSeconds(second));
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start...");
+            Thread(10);
+            Console.WriteLine("End...");
+        }
+    }
+}
